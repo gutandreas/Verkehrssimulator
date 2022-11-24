5bf75dfea9b3b95babe85b02179094421c603941
Andreas Gut <andreasgut@gmx.ch>
2022-11-24 15:59:27 +0100

    Ampelbilder können Farben wechseln ohne Funktion

--- main.py
+++ main.py
@@ -33,11 +33,17 @@ CAR_PICTURE_GREEN = pygame.transform.scale(CAR_PICTURE_GREEN, CAR_DIMENSIONS)
 
 CAR_PICTURES = [CAR_PICTURE_RED, CAR_PICTURE_YELLOW, CAR_PICTURE_BLUE, CAR_PICTURE_GREEN, CAR_PICTURE_CYAN]
 
-SIGNAL = pygame.image.load(os.path.join("bilder", "ampel_rot.png"))
-SIGNAL = pygame.transform.scale(SIGNAL, SIGNAL_DIMENSIONS)
+SIGNAL_PICTURE_RED = pygame.image.load(os.path.join("bilder", "ampel_rot.png"))
+SIGNAL_PICTURE_RED = pygame.transform.scale(SIGNAL_PICTURE_RED, SIGNAL_DIMENSIONS)
+
+SIGNAL_PICTURE_GREEN = pygame.image.load(os.path.join("bilder", "ampel_gruen.png"))
+SIGNAL_PICTURE_GREEN = pygame.transform.scale(SIGNAL_PICTURE_GREEN, SIGNAL_DIMENSIONS)
 
 CARS = pygame.sprite.Group()
 SIGNALS = pygame.sprite.Group()
+SIGNALS_POS_1 = []
+SIGNALS_POS_2 = []
+SIGNALS_POS_3 = []
 
 #start4 = [width / 2.05, -50]
 
@@ -48,35 +54,68 @@ distance = 15
 class Signal(pygame.sprite.Sprite):
   def __init__(self, position, direction):
     pygame.sprite.Sprite.__init__(self)
+    self.direction = direction
 
     SIGNALS.add(self)
 
     signal_distance = 100
 
     if position == 1:
-      self.position = [width*0.317, height*0.465]
+      self.position = [width*0.318, height*0.465]
     elif position == 2:
-      self.position = [width * 0.48, height * 0.465]
+      self.position = [width * 0.485, height * 0.465]
     elif position == 3:
       self.position = [width * 0.65, height * 0.465]
 
     if direction == 1:
-      self.position = [self.position[0] - signal_distance*0.75, self.position[1] - signal_distance*0.9]
-      self.picture = pygame.transform.rotate(SIGNAL, 180)
+      self.position = [self.position[0] - signal_distance*0.65, self.position[1] - signal_distance*0.8]
+      self.picture = pygame.transform.rotate(SIGNAL_PICTURE_RED, 180)
     elif direction == 2:
-      self.position = [self.position[0] + signal_distance*0.8, self.position[1] - signal_distance*0.6]
-      self.picture = pygame.transform.rotate(SIGNAL, 90)
+      self.position = [self.position[0] + signal_distance*0.85, self.position[1] - signal_distance*0.5]
+      self.picture = pygame.transform.rotate(SIGNAL_PICTURE_RED, 90)
     elif direction == 3:
-      self.position = [self.position[0] + signal_distance*0.8, self.position[1] + signal_distance*0.95]
-      self.picture = pygame.transform.rotate(SIGNAL, 0)
+      self.position = [self.position[0] + signal_distance*0.85, self.position[1] + signal_distance*1]
+      self.picture = pygame.transform.rotate(SIGNAL_PICTURE_RED, 0)
     elif direction == 4:
-      self.position = [self.position[0] - signal_distance*1, self.position[1] + signal_distance*0.95]
-      self.picture = pygame.transform.rotate(SIGNAL, 270)
+      self.position = [self.position[0] - signal_distance*0.95, self.position[1] + signal_distance*1]
+      self.picture = pygame.transform.rotate(SIGNAL_PICTURE_RED, 270)
 
     self.rect = self.picture.get_rect()
     self.rect.x = self.position[0]
     self.rect.y = self.position[1]
 
+  def change_color(self, color):
+    self.picture = SIGNAL_PICTURE_GREEN
+    print("green")
+
+    if self.direction == 1:
+      if color == "green":
+        self.picture = SIGNAL_PICTURE_GREEN
+        self.picture = pygame.transform.rotate(SIGNAL_PICTURE_GREEN, 180)
+      else:
+        self.picture = SIGNAL_PICTURE_RED
+        self.picture = pygame.transform.rotate(SIGNAL_PICTURE_RED, 180)
+    elif self.direction == 2:
+      if color == "green":
+        self.picture = SIGNAL_PICTURE_GREEN
+        self.picture = pygame.transform.rotate(SIGNAL_PICTURE_GREEN, 270)
+      else:
+        self.picture = SIGNAL_PICTURE_RED
+        self.picture = pygame.transform.rotate(SIGNAL_PICTURE_RED, 270)
+    elif self.direction == 3:
+      if color == "green":
+        self.picture = SIGNAL_PICTURE_GREEN
+        self.picture = pygame.transform.rotate(SIGNAL_PICTURE_GREEN, 0)
+      else:
+        self.picture = SIGNAL_PICTURE_RED
+        self.picture = pygame.transform.rotate(SIGNAL_PICTURE_RED, 0)
+    elif self.direction == 4:
+      if color == "green":
+        self.picture = SIGNAL_PICTURE_GREEN
+        self.picture = pygame.transform.rotate(SIGNAL_PICTURE_GREEN, 90)
+      else:
+        self.picture = SIGNAL_PICTURE_RED
+        self.picture = pygame.transform.rotate(SIGNAL_PICTURE_RED, 90)
 
 
 class Car(pygame.sprite.Sprite):
@@ -84,28 +123,28 @@ class Car(pygame.sprite.Sprite):
   def __init__(self, start):
     pygame.sprite.Sprite.__init__(self)
     if start == 1:
-      start = [width * 0.3, -50]
+      start = [width * 0.31, -50]
       self.direction = 1
     elif start == 2:
-      start = [width * 0.47, -50]
+      start = [width * 0.475, -50]
       self.direction = 1
     elif start == 3:
-      start = [width * 0.635, -50]
+      start = [width * 0.64, -50]
       self.direction = 1
     elif start == 4:
-      start = [width, height * 0.45]
+      start = [width, height * 0.47]
       self.direction = 2
     elif start == 5:
-      start = [width * 0.67, height + 50]
+      start = [width * 0.68, height + 50]
       self.direction = 3
     elif start == 6:
-      start = [width * 0.5, height + 50]
+      start = [width * 0.515, height + 50]
       self.direction = 3
     elif start == 7:
-      start = [width * 0.34, height + 50]
+      start = [width * 0.345, height + 50]
       self.direction = 3
     else:
-      start = [-50, height * 0.51]
+      start = [-50, height * 0.53]
       self.direction = 4
 
     if self.direction == 1:
@@ -178,9 +217,18 @@ def main():
   running = True
   clock = pygame.time.Clock()
 
-  for i in range(1,4,1):
-    for j in range(1,5,1):
-      Signal(i, j)
+
+  for j in range(1,5,1):
+    s = Signal(1, j)
+    SIGNALS_POS_1.append(s)
+
+  for j in range(1,5,1):
+    s = Signal(2, j)
+    SIGNALS_POS_2.append(s)
+
+  for j in range(1,5,1):
+    s = Signal(3, j)
+    SIGNALS_POS_3.append(s)
 
   counter = 0
 
@@ -190,8 +238,24 @@ def main():
     clock.tick(FPS)
 
     if counter == 0:
+      global SPRITES
+      print(len(CARS))
       for i in range(1,9,1):
         Car(i)
+
+    if counter == 20:
+      pos_1 = random.randrange(0,4)
+      SIGNALS_POS_1[pos_1].change_color("green")
+      pos_2 = random.randrange(0,4)
+      SIGNALS_POS_2[pos_2].change_color("green")
+      pos_3 = random.randrange(0, 4)
+      SIGNALS_POS_3[pos_3].change_color("green")
+
+
+    if counter == 30:
+      SIGNALS_POS_1[pos_1].change_color("red")
+      SIGNALS_POS_2[pos_2].change_color("red")
+      SIGNALS_POS_3[pos_3].change_color("red")
 
     counter += 1
 
